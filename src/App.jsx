2d00909a537fbda9b06748327ed00c5aec36e646
Ervigio <ejadd_75@hotmail.com>
2023-11-17 00:24:09 +0000

    Enlazar Ejercicio4 a la app principal: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Ejercicio1 from './components/Ejercicio1/Ejercicio1'
 import Ejercicio2 from './components/Ejercicio2/Ejercicio2'
 import Ejercicio3 from './components/Ejercicio3/Ejercicio3'
+import Ejercicio4 from './components/Ejercicio4/Ejercicio4'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import TituloPrincipal from './components/TituloPrincipal'
@@ -15,7 +16,7 @@ export default function App() {
         <Ejercicio1 />
         <Ejercicio2 />
         <Ejercicio3 />
-
+        <Ejercicio4 />
       </main>
       <Footer />
     </>
