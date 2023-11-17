e8bee3ef7792f412b6de757e93fc1c65c973993a
Ervigio <ejadd_75@hotmail.com>
2023-11-17 00:23:18 +0000

    Estructurar archivo Ejercicio4

---
+++ src/components/Ejercicio4/Ejercicio4.jsx
@@ -0,0 +1,13 @@
+import Ejercicio4 from "../Ejercicio4/Ejercicio4";
+import TituloEjercicio from "../TituloEjercicio";
+import Contador from "../Ejercicio4/Contador"
+
+export default function Ejercicio4() {
+    return (
+        <section>
+            <TituloEjercicio> Éste es el Ejercicio 4</TituloEjercicio>
+            <Contador>Contador</Contador>
+            <Botón>Pulsa aquí</Botón>
+        </section>
+    )
+}
